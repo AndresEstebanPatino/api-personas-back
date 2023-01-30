694f8c5d38044fb2f820d72d67e6d0c801e8061a
BCN MAC 021 <bcnmac021@MacBook-Pro-de-BCN.local>
2023-01-30 10:48:07 +0100

    first commit

---
+++ database/factories/PersonaFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Persona>
+ */
+class PersonaFactory extends Factory
+{
+
+    public function definition()
+    {
+        return [
+            'nombre' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            
+        ];
+    }
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
